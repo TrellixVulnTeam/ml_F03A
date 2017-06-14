cafbd3366bab1810615bfc538377f2848ea79d0c
Eskil J <eskil@vuzii.com>
2017-06-14 22:00:09 +0200

    removed config files

--- trainer/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-PROJECT_ID=$(gcloud config list project --format "value(core.project)")
-BUCKET_NAME=${PROJECT_ID}-mlengine
-BUCKET_ADR=gs://${BUCKET_NAME}/data
-
-gsutil cp -r data $BUCKET_ADR/flowers
